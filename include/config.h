641fdd3e4702fe9d1c256ff07afcfe3ab8a365ba
Lankow <maciej.lankow@gmail.com>
2023-09-24 12:15:05 +0200

    Added config.g file - to be commited only once and used as holder for secrets

---
+++ include/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define WIFI_SSID "enter_wifi_ssid_here";
+#define WIFI_PASSWORD "enter_wifi_password_here";
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "config.h"
+
+#define LED_BUILTIN 2
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+WiFiServer server(80);
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(921600);
   Serial.println("Setup");
 }
