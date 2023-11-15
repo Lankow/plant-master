be56adc3c62d27db7639293e3057c2a0dbd6fa70
Lankow <maciej.lankow@gmail.com>
2023-11-16 00:17:11 +0100

    Vite React App Initialized and PlatformoIO config adjusted

--- scripts/build_react_app.py
+++
@@ -1,7 +0,0 @@
-Import("env")
-import os
-
-def before_build_spiffs(source, target, env):
-    print("Building plant-master-react...") 
-
-env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
