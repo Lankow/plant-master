ef23e23883ea78f942142ed1443a573ecc664292
Lankow <maciej.lankow@gmail.com>
2023-11-30 20:50:26 +0100

    Added Loading Icon when data is being fetched

--- plant-master-client/src/App.tsx
+++ plant-master-client/src/App.tsx
@@ -1,21 +1,24 @@
 import { IMessageEvent, w3cwebsocket } from "websocket"
 import { useCallback, useEffect, useRef, useState } from "react"
-import { Box, Container, CssBaseline, Grid } from "@mui/material"
+import { Box, CircularProgress, Container, CssBaseline, Grid } from "@mui/material"
 import HumiditySensor from "./HumiditySensor"
 import { Footer } from "./Footer"
 import TopBar from "./TopBar"
 import Modal from "./Modal"
 
-function App() {
-  const debugMode: boolean = true
-  const initialPin: number = 0
-  const initialThreshold: number = 4000
+const debugMode = true
+const initialPin = 0
+const initialThreshold = 4000
+const serverAddress = "ws://192.168.1.184/ws"
 
+function App() {
   const websocket = useRef<w3cwebsocket | null>(null)
   const [editedThreshold, setEditedThreshold] = useState(initialThreshold)
   const [editedPin, setEditedPin] = useState(initialPin)
   const [editedId, setEditedId] = useState(0xff)
   const [openedModal, setOpenedModal] = useState("")
+  const [dataReceived, setDataReceived] = useState(false)
+  const [sensors, setSensors] = useState(Array.from({ length: 8 }, () => ({ id: 0xff, lvl: 0, threshold: 0, active: false })))
 
   const resetInputs = useCallback(() => {
     setEditedPin(initialPin)
@@ -32,22 +35,14 @@ function App() {
 
   const handleCloseModal = useCallback(() => {
     setOpenedModal("")
-  }, [setOpenedModal])
+  }, [])
 
   const sendUpdate = useCallback(({ id, threshold, pin }: { id: number; threshold?: number; pin?: number }) => {
     const updatedData: { type: string; id: number; threshold?: number; pin?: number } = {
       type: "edit",
       id,
-      threshold: undefined,
-      pin: undefined
-    }
-
-    if (threshold !== initialThreshold) {
-      updatedData.threshold = threshold
-    }
-
-    if (pin !== initialPin) {
-      updatedData.pin = pin
+      threshold: threshold !== initialThreshold ? threshold : undefined,
+      pin: pin !== initialPin ? pin : undefined
     }
 
     websocket.current?.send(JSON.stringify(updatedData))
@@ -71,42 +66,43 @@ function App() {
   }, [editedId, editedThreshold, editedPin, sendUpdate, handleCloseModal])
 
   useEffect(() => {
-    websocket.current = new w3cwebsocket("ws://192.168.1.184/ws")
+    websocket.current = new w3cwebsocket(serverAddress)
     websocket.current.onmessage = (message: IMessageEvent) => {
       const dataFromServer = JSON.parse(message.data.toString())
-      setSensors(dataFromServer.humidity)
+      if (dataFromServer.humidity) {
+        setSensors(dataFromServer.humidity)
+        setDataReceived(true)
+      } else {
+        setDataReceived(false)
+      }
     }
     return () => websocket.current?.close()
   }, [])
-
-  const initialSensors = Array.from({ length: 8 }, () => ({
-    id: 0xff,
-    lvl: 0,
-    threshold: 0,
-    active: false
-  }))
-  const [sensors, setSensors] = useState(initialSensors)
 
   return (
     <Box id="root" sx={{ background: "#8ebeed", height: "100%" }}>
       <CssBaseline />
       <TopBar handleOpenModal={handleOpenModal} handleGetLogs={handleGetLogs} />
-      <Container sx={{ p: 2 }}>
-        <Grid container spacing={3}>
-          {sensors.map((sensor, index) =>
-            sensor.active || debugMode ? (
-              <HumiditySensor
-                key={index} //
-                id={sensor.id}
-                lvl={sensor.lvl}
-                threshold={sensor.threshold}
-                active={sensor.active}
-                handleOpenModal={handleOpenModal}
-                setEditedId={setEditedId}
-              />
-            ) : null
-          )}
-        </Grid>
+      <Container sx={{ p: 2, display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
+        {dataReceived ? (
+          <Grid container spacing={3}>
+            {sensors.map((sensor, index) =>
+              sensor.active || debugMode ? (
+                <HumiditySensor //
+                  key={index}
+                  id={sensor.id}
+                  lvl={sensor.lvl}
+                  threshold={sensor.threshold}
+                  active={sensor.active}
+                  handleOpenModal={handleOpenModal}
+                  setEditedId={setEditedId}
+                />
+              ) : null
+            )}
+          </Grid>
+        ) : (
+          <CircularProgress size="10rem" />
+        )}
       </Container>
       <Footer />
       <Modal
