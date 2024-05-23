9316932ee2fe15170d17f35ab2ec71cc55e5da06
Lankow <maciej.lankow@gmail.com>
2024-05-23 21:05:38 +0200

    Creates Access Point on start.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
 #include <Wire.h>  
 #include "SSD1306Wire.h"
 #include "OLEDDisplayUi.h"
+#include <Preferences.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
 #define INITIAL_SCREEN_DURATION 3000
+#define CONFIG_PAGE_SSID "config_plant_master"
+#define CONFIG_PAGE_PASSWORD "plantmaster"
+
+IPAddress ip(192, 168, 4, 1);
+IPAddress netmask(255, 255, 255, 0);
 
 SSD1306Wire display(0x3c, SDA, SCL); 
+WebServer servidor(80);
 
 void drawInitialScreen()
 {
@@ -13,6 +23,23 @@ void drawInitialScreen()
   display.drawHorizontalLine(10, 26, 100);
   display.drawString(35, 30, "by Lankow");
   display.display();
+}
+
+void drawConfigScreen(){
+  display.clear();
+  display.drawString(10,0, "Config SSID:");
+  display.drawString(10,10, CONFIG_PAGE_SSID);
+  display.drawHorizontalLine(10, 24, 100);
+  display.drawString(10,25, "Config PASSWORD:");
+  display.drawString(10,35, CONFIG_PAGE_PASSWORD);
+  display.display();
+}
+
+void setupAccessPoint(){
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ip, ip, netmask);
+    WiFi.softAP(CONFIG_PAGE_SSID, CONFIG_PAGE_PASSWORD);
+    servidor.begin();
 }
 
 void setup()
@@ -24,8 +51,7 @@ void setup()
 
 void loop()
 {
-  display.clear();
-  display.display();
-
+  drawConfigScreen();
+  setupAccessPoint();
   delay(1000);
 }
