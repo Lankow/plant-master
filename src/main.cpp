0ea51685d4381227a4d8584a1fd7275bbe59839d
Lankow <maciej.lankow@gmail.com>
2024-05-23 19:38:44 +0200

    Initial screen displays plant master text.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,30 @@
 #include <Wire.h>  
 #include "SSD1306Wire.h"
 #include "OLEDDisplayUi.h"
+#define INITIAL_SCREEN_DURATION 3000
 
 SSD1306Wire display(0x3c, SDA, SCL); 
-  
+
+void drawInitialScreen()
+{
+  display.clear();
+  display.drawString(30, 10, "Plant-Master");
+  display.drawHorizontalLine(10, 26, 100);
+  display.drawString(35, 30, "by Lankow");
+  display.display();
+}
+
 void setup()
 {
   display.init();
-  display.clear();
-  display.drawString(30, 30, "Plant-Master");
-  display.display();
+  drawInitialScreen();
+  delay(INITIAL_SCREEN_DURATION);
 }
 
 void loop()
 {
+  display.clear();
+  display.display();
+
   delay(1000);
 }
