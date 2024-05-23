def73be5dd6dbf0f4239fa61cdd3818e4d2ba3c2
Lankow <maciej.lankow@gmail.com>
2024-05-23 18:48:03 +0200

    SSD1306 drivers added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <Arduino.h>
+#include <Wire.h>  
+#include "SSD1306Wire.h"
+#include "OLEDDisplayUi.h"
 
+SSD1306Wire display(0x3c, SDA, SCL); 
+  
 void setup()
 {
-  Serial.begin(921600);
+  display.init();
+  display.clear();
+  display.drawString(30, 30, "Plant-Master");
+  display.display();
 }
 
 void loop()
