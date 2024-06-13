fb65669300279abcb3a99b5fc908056e3dcdfc38
Lankow <maciej.lankow@gmail.com>
2024-06-13 21:13:01 +0200

    Script determines which config shall be used and version of the project.

--- scripts/prepare_config.py
+++ scripts/prepare_config.py
@@ -1,11 +1,30 @@
-import os
+Import("env")
 
-def verifyConfig(defines):
-    print(defines)
-    print("Verifying config file...")
+def initializeConfig():
+    print("Initializing configuration...")
+    cpp_defines = env.ParseFlags(env['BUILD_FLAGS']).get("CPPDEFINES")
+    
+    # Initialize version variable
+    version_value = None
 
-def moveConfig():
-    print("Moving config file...")
+    # Loop through the list to find 'VERSION'
+    for item in cpp_defines:
+        if isinstance(item, list) and item[0] == 'VERSION':
+            version_value = item[1]
+            break
+
+    print(version_value)  # Output should be '1.0'
+
+    if("PLANT_MASTER" in cpp_defines):
+        config_file_name = "config-master.json"
+    else:
+        config_file_name = "config-monitor.json"
+    
+    print("Chosen config file:" + config_file_name)
+
+
+
+
 
 def createConfig():
     print("Creating config file...")
@@ -13,5 +32,6 @@ def createConfig():
 def setDefaultConfig():
     print("Setting default values for config file...")
 
-build_flags = os.getenv('BUILD_FLAGS')
-print(build_flags)
+
+
+initializeConfig()
