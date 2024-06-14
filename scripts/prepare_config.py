1d2162ba19fd5e647669a40884a01025e2f12cb7
Lankow <maciej.lankow@gmail.com>
2024-06-14 23:34:27 +0200

    Changed approach for config.

--- scripts/prepare_config.py
+++ scripts/prepare_config.py
@@ -1,37 +1,63 @@
 Import("env")
+import os
+import shutil
 
-def initializeConfig():
+def handle_config():
     print("Initializing configuration...")
     cpp_defines = env.ParseFlags(env['BUILD_FLAGS']).get("CPPDEFINES")
-    
-    # Initialize version variable
+    version = get_version(cpp_defines)
+    config_file_path = "config.json"
+
+    if os.path.isfile(config_file_path):
+        validate_config()
+    else:
+        print(f"{config_file_path} not found in parent directory.")
+        generate_default_config()
+
+    # Determine the config file name based on a specific define
+    if "PLANT_MASTER" in cpp_defines:
+        output_config_path = "config-master.json"
+    else:
+        output_config_path = "config-monitor.json"
+    print("Config File: " + output_config_path)
+
+    move_config()
+
+
+def get_version(cpp_defines):
     version_value = None
 
-    # Loop through the list to find 'VERSION'
     for item in cpp_defines:
         if isinstance(item, list) and item[0] == 'VERSION':
             version_value = item[1]
+            print("Config Version: " + version_value)
             break
 
-    print(version_value)  # Output should be '1.0'
-
-    if("PLANT_MASTER" in cpp_defines):
-        config_file_name = "config-master.json"
-    else:
-        config_file_name = "config-monitor.json"
-    
-    print("Chosen config file:" + config_file_name)
+    print("Config Version not found.")
+    return version_value
 
 
+def generate_default_config():
+    print("Generating default configuration...")
 
 
-
-def createConfig():
-    print("Creating config file...")
-
-def setDefaultConfig():
-    print("Setting default values for config file...")
+def validate_config():
+    print("Validating configuration...")
 
 
+def move_config(output_config_path):
+    print("Moving configuration...")
+    data_dir = os.path.join(os.getcwd(), "data")
 
-initializeConfig()
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    dest_path = os.path.join(data_dir, output_config_path)
+
+    shutil.copy(config_file_path, dest_path)
+    print(f"Modified {config_file_path} copied as {dest_path}")
+
+print("---------------------")
+handle_config()
+print("---------------------")
+
