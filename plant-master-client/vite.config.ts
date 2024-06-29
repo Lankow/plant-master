15bb9fe2417b2f0d43e7fc02cf80824eb43ab9af
Lankow <maciej.lankow@gmail.com>
2024-06-29 13:04:18 +0200

    Env production test.

--- plant-master-client/vite.config.ts
+++ plant-master-client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/plant-master/',
+  base: process.env.BASE_PATH || '/',
   plugins: [react()],
   build: {
     emptyOutDir: true,
